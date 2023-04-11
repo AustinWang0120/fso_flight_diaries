5de2eed0ab2a17097787c8a9650aac84dc02b8b6
Austin Wang <yyaustin@bu.edu>
2023-04-11 04:42:58 -0400

    Setup data

---
+++ build/src/services/diaryService.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const entries_1 = __importDefault(require("../../entries"));
+const getEntries = () => {
+    return entries_1.default;
+};
+const getNonSensitiveEntries = () => {
+    return entries_1.default.map(({ id, date, weather, visibility }) => ({
+        id, date, weather, visibility
+    }));
+};
+const addDiary = () => {
+    return null;
+};
+const myService = {
+    getEntries,
+    getNonSensitiveEntries,
+    addDiary
+};
+exports.default = myService;
